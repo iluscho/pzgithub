c9d63045a924172fdde3a144249e655bbbfd9712
asfasdfgasdfasdf@outlook.com <asfasdfgasdfasdf@outlook.com>
2025-02-04 16:01:10 +0500

    Добавьте файлы проекта.

---
+++ pzgithub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pzgithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is the first repo!");
+        }
+    }
+}
